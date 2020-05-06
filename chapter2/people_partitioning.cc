17e056bde328287211742ca6ff5bd8c7ce0f6522
Tuan Tran <tuantran@Tuans-MacBook-Pro.local>
2020-05-06 15:59:28 +0700

    finish chapter2

--- chapter2/people_partitioning.cc
+++ chapter2/people_partitioning.cc
@@ -9,6 +9,7 @@ struct Person {
     bool isMale;
 
     Person(std::string name, bool isMale): name(name), isMale(isMale) {}
+
     friend std::ostream& operator<<(std::ostream& os, const Person& p);
 };
 
@@ -19,22 +20,35 @@ std::ostream& operator<<(std::ostream& os, const Person& p) {
 
 struct People: public std::vector<Person> {
     People(std::initializer_list<Person>&& lst) {
-        std::for_each(lst.begin(), lst.end(), [this] (const Person& p) {
+        std::for_each(lst.begin(), lst.end(), [this] (const Person& p) -> bool {
             this->push_back(p);
             return true;
         });
     }
 
-    People::iterator partition() {
-        return std::partition(this->begin(), this->end(), [] (const Person& p) {
+    People::iterator partition_female() {
+        return std::partition(this->begin(), this->end(), [] (const Person& p) -> bool {
             return !p.isMale;
         });
     }
 
-    People::iterator stable_partition() {
-        return std::stable_partition(this->begin(), this->end(), [] (const Person& p) {
+    People::iterator stable_partition_female() {
+        return std::stable_partition(this->begin(), this->end(), [] (const Person& p) -> bool {
             return !p.isMale;
         });
+    }
+
+    std::vector<std::string> female_names() {
+        std::vector<Person> females;
+        std::copy_if(this->cbegin(), this->cend(), std::back_inserter(females), [] (const Person& p) -> bool {
+            return !p.isMale;
+        });
+
+        std::vector<std::string> result(females.size());
+        std::transform(females.cbegin(), females.cend(), result.begin(), [] (const Person& p) -> std::string {
+            return p.name;
+        });
+        return result;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const People& p);
@@ -42,7 +56,7 @@ struct People: public std::vector<Person> {
 
 std::ostream& operator<<(std::ostream& os, const People& p) {
     return std::accumulate(p.cbegin(), p.cend(), std::ref(os),
-        [] (std::ostream& os_, Person person) -> std::ostream& {
+        [] (std::ostream& os_, const Person& person) -> std::ostream& {
             os_ << person << std::endl;
             return os_;
         }
@@ -54,14 +68,25 @@ int main(void) {
                         Person("David", true), Person("Rose", false), Person("Tom", true) };
     std::cout << people << std::endl;
     std::cout << "======================" << std::endl;
-    people.partition();
+    people.partition_female();
     std::cout << people << std::endl;
     std::cout << "======================" << std::endl;
 
     People people2 = { Person("Peter", true), Person("Martha", false), Person("Jane", false),
                         Person("David", true), Person("Rose", false), Person("Tom", true) };
-    people2.stable_partition();
+    people2.stable_partition_female();
     std::cout << people2 << std::endl;
     std::cout << "======================" << std::endl;
+
+    People people3 = { Person("Peter", true), Person("Martha", false), Person("Jane", false),
+                        Person("David", true), Person("Rose", false), Person("Tom", true) };
+    std::vector<std::string> names = people3.female_names();
+    std::accumulate(names.cbegin(),
+        names.cend(),
+        std::ref(std::cout),
+        [] (std::ostream& os, const std::string& name) -> std::ostream& {
+        os << name << " ";
+        return os;
+    });
     return 0;
 }
